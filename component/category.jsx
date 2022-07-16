fd8388fcf2e8dc5ac7b338f46c517799fbd8d73a
Rossi Kamal <rossikamal@gmail.com>
2022-07-17 02:10:43 +0600

    Update category.jsx

--- component/category.jsx
+++ component/category.jsx
@@ -5,13 +5,10 @@ const Category = () => {
     return (
 
         <div className="category-container">
-            <CategorySingle href='/search?category=smart phone' name='Smart Phone' />
-            <CategorySingle href='/search?category=desktop' name='Desktop' />
-            <CategorySingle href='/search?category=watch' name='Watch' />
-            <CategorySingle href='/search?category=ac' name='Smart Ac' />
-            <CategorySingle href='/search?category=motor bike' name='Motor Bike' />
-            <CategorySingle href='/search?category=tv' name='Smart tv & Android Tv' />
-            <CategorySingle href='/search?category=laptop' name='Laptop' />
+            <CategorySingle href='/search?category=men' name='Men' />
+            <CategorySingle href='/search?category=women' name='Women' />
+            <CategorySingle href='/search?category=kid' name='Kid' />
+           
         </div>
     )
 }
